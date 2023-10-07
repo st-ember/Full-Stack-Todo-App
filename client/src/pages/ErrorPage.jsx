b7966dfa4e54788a1e231bd5bca293abb4fb3cf9
st-ember <86178430+st-ember@users.noreply.github.com>
2023-10-07 21:31:41 +0800

    Add files via upload
    added error page

---
+++ client/src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ErrorPage() {
+    return (
+        <div
+        className='text-5xl w-fit m-auto my-20'
+        >Sorry, we don't have this page yet...</div>
+    );
+};
+
+export default ErrorPage;
