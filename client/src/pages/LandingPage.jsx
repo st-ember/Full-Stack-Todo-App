81c2a6e965ea524cb983bba77651dec6325d71a7
st-ember <86178430+st-ember@users.noreply.github.com>
2023-10-07 16:32:21 +0800

    Update LandingPage.jsx
    Changed h1 to better reflect app identity
    Added flex to "create account" link to improve rwd

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -9,17 +9,16 @@ function LandingPage() {
         grid grid-cols-2'>
             <div className='grid grid-cols-1 py-60 px-32'>
                 <h1 className='text-black text-5xl'>
-                    Welcome To This App
+                    Create your own todo list
                 </h1>
                 <h2 className='text-black text-3xl my-6'>
                     Sign in or create an account
                 </h2>
             </div>
-            <div
-            className='bg-neutral-700 py-60 pl-20'>
+            <div className='bg-neutral-700 py-60 pl-20'>
                 <Link className='p-4 text-2xl hover:cursor-pointer hover:text-amber-200 active:text-amber-400'
                 to="/signin">Sign In</Link>
-                <Link className='p-4 text-2xl hover:cursor-pointer hover:text-amber-200 active:text-amber-400'
+                <Link className='p-4 text-2xl hover:cursor-pointer hover:text-amber-200 active:text-amber-400 flex'
                 to="/signup">Create Account</Link>
             </div>
         </div>
